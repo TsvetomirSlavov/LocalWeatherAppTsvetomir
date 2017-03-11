8a0a3ec6902e52f132724339e463d9bceddd8a53
TsvetomirSlavov <tslavov@yahoo.com>
2017-03-11 00:56:40 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var APPID = "dcc542a87c0298edefc39beceac09033";
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+
+function updateByZip(zip) {
+	var url = "http://api.openweathermap.org/data/2.5/weather?" + 
+		"zip=" + zip + 
+		"&APPID=" + APPID;
+	sendRequest(url);
+}
+
+function sendRequest(url){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			var data = JSON.parse(xmlhttp.responseText);
+			var weather = {};
+			weather.icon = data.weather[0].id;
+			weather.humidity = data.main.humidity;
+			weather.wind = data.wind.speed;
+			weather.direction = data.wind.deg;
+			weather.loc = data.name;
+			weather.temp = data.main.temp;
+			update(weather);
+			console.log(weather.temp, weather.loc, weather.humidity, weather.icon);
+		 }
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+function update(weather) {
+	wind.innerHTML = weather.wind;
+	direction.innerHTML = weather.direction;
+	humidity.innerHTML = weather.humidity;
+	loc.innerHTML = weather.loc;
+	temp.innerHTML  = weather.temp;
+	icon.src = "imgs/codes/" + weather.icon + ".png";
+	console.log(weather.temp, weather.loc, weather.humidity, weather.icon);
+}
+
+window.onload = function () {
+	temp = document.getElementById("temperature");
+	loc = document.getElementById("location");
+	icon = document.getElementById("icon");
+	humidity = document.getElementById("humidity");
+	wind = document.getElementById("wind");
+	direction = document.getElementById("direction");	
+	
+	updateByZip(60016);
+}
